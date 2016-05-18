7d92795d08ee9c173cf36356742360048fd2afd8
Amali Lakshika <maamalilakshika@gmail.com>
2016-05-18 10:59:18 +0530

    Add Product Controller

--- app/controller/product_controller.js
+++ app/controller/product_controller.js
@@ -1,20 +1,16 @@
 /**
- * Created by Himashi Nethinika on 5/13/2016.
+ * Created by Amali Lakshika on 5/18/2016.
  */
 
 module.controller('ProductController', ProductController);
-
 function ProductController($scope, ProductService) {
-
     $scope.saveCustomer = function () {
         IndividualCustomerService.saveCustomer($scope.customer).then(function (data) {
             if (data.text == "200") {
-                Window.alert("Save Successfully..");
+                Window.alert("successfully saved..");
             } else {
-                Window.alert("Saving Fail..");
+                Window.alert("Saving Failed..");
             }
         });
     };
-
-   
 };
